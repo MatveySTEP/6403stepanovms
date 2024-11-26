c7198aef5a6d47d57adb6ae5bcf472dee058bda2
Stepanov <m.stepanov2@eapteka.ru>
2024-11-26 15:22:27 +0400

    Выполнен рефактор, выполнена вторая лабораторная работа

---
+++ data_analysis/service/main.py
@@ -0,0 +1,22 @@
+import time
+
+from data_analysis.service.realtime_data_loader import RealtimeMonitoringService
+
+service = RealtimeMonitoringService(1, [], 1000, '../configs/logging.conf')
+service.start()
+
+print("Введите сообщение (для выхода введите 'exit' или 'quit'):")
+
+while True:
+    cities= {'vancuver': [49.2497, -123.1193], 'chicago': [41.85, 87.65], 'argentina': [3.1412, 101.687]}
+    # Читаем сообщение от пользователя
+    message = input()
+    # Проверяем, не ввел ли пользователь команду для выхода
+    if message.lower() in ['exit', 'quit']:
+        print("Спасибо за использование программы. До свидания!")
+        service.stop()
+        break
+    if message.lower() in cities:
+        service.set_coords(cities[message.lower()])
+    # Выводим сообщение на экран
+    print(f"Получено сообщение: {message}")
